ea3a41603196611e2b36f6ee472a2506d8177a6e
prakher01 <psaxena212@gmail.com>
2026-06-22 18:37:27 +0530

    update_next_config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  env: {
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
 };
 
 export default nextConfig;
